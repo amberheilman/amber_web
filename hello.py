7a828fbc0c3191acbdcc2a23a59335d2e5bb30ce
Amber Heilman <amber.l.heilman@gmail.com>
2014-03-09 18:19:09 -0400

    hexagon css layout complete

--- hello.py
+++ hello.py
@@ -1,8 +1,12 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Hello World!'
+def home():
+    return render_template('home.html', name='home')
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('page_not_found.html'), 404
