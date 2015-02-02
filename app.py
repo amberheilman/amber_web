e39e87f24910e865e20ef8fca11a8ca5d02310a4
Amber Heilman <amber@localhost.localdomain>
2015-02-01 22:24:01 -0500

    layout for skills created

--- app.py
+++ app.py
@@ -7,9 +7,16 @@ app = Flask(__name__)
 def index():
     return render_template('index.html', name='index')
 
+
+@app.context_processor
+def inject_skillset():
+    return dict(skillset={'background-image: url("static/python.png"); height:280px; width:280px;': 'Creating RESTful APIs with Python 2, 3',
+    					  'background-image: url("static/jenkins.png"); height:280px; width:280px; background-color: black;': 'Deployment'})
+
+
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template('page_not_found.html'), 404
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
